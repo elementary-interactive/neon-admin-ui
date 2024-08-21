be04fc4cecf790d11c27d98026865111038f31f9
Balazs Ercsey <laze@laze.hu>
2024-08-21 14:25:40 +0200

    Fix version numbers.

--- src/NeonAdminServiceProvider.php
+++ src/NeonAdminServiceProvider.php
@@ -21,7 +21,7 @@ use Spatie\LaravelPackageTools\Package;
 
 class NeonAdminServiceProvider extends PackageServiceProvider
 {
-  const VERSION = '3.0.0-alpha-30';
+  const VERSION = '3.0.0-alpha-31';
 
   public function configurePackage(Package $package): void
   {
