47e139aa71297ee3f25d4550acee9fae5bc62eaa
Egon <egonstefan@pm.me>
2024-10-18 13:30:46 +0200

    teszt latest filament

--- src/NeonAdminServiceProvider.php
+++ src/NeonAdminServiceProvider.php
@@ -21,7 +21,7 @@ use Spatie\LaravelPackageTools\Package;
 
 class NeonAdminServiceProvider extends PackageServiceProvider
 {
-  const VERSION = '3.0.0-alpha-37';
+  const VERSION = '3.0.0-alpha-dev';
 
   public function configurePackage(Package $package): void
   {
